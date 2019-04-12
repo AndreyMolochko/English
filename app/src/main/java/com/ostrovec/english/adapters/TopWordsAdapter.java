68d6ed00babe721d3b3b51c14d646fbd49d16b4f
andrey.molochko <andrey.molochko@exposit.com>
2019-04-12 01:47:41 -0700

    link view via butter knife in adapter

--- app/src/main/java/com/ostrovec/english/adapters/TopWordsAdapter.java
+++ app/src/main/java/com/ostrovec/english/adapters/TopWordsAdapter.java
@@ -5,13 +5,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.ostrovec.english.R;
 
+import org.w3c.dom.Text;
+
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class TopWordsAdapter extends BaseAdapter {
+
+    @BindView(R.id.item_grid_view_name_text_view)
+    TextView textView;
+
+    @BindView(R.id.item_grid_view_bronze_medal_image_view)
+    ImageView bronzeMedalView;
+
+    @BindView(R.id.item_grid_view_silver_medal_image_view)
+    ImageView silverMedalView;
+
+    @BindView(R.id.item_grid_view_gold_medal_image_view)
+    ImageView goldMedalView;
+
+    @BindView(R.id.item_grid_view_progress_bar)
+    ProgressBar progressBar;
+
+    @BindView(R.id.item_grid_view_value_progress_text_view)
+    TextView valueProgressValue;
+
+
 
     private List<String> tops;
     private Context context;
@@ -40,16 +67,14 @@ public class TopWordsAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
 
         String name = tops.get(position);
-
         if(convertView == null){
             final LayoutInflater layoutInflater = LayoutInflater.from(context);
             convertView = layoutInflater.inflate(R.layout.top_words_item_grid_view,null);
         }
-
-        TextView textView = convertView.findViewById(R.id.item_grid_view_name_text_view);
+        ButterKnife.bind(this,convertView);
         textView.setText(name);
-
-
+        progressBar.setProgress(30);
+        valueProgressValue.setText("30%");
 
         return convertView;
     }
